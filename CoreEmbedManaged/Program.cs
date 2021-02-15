9b4eb8400ba14565ffc02a89c17c67104ae49c06
alin <alinoctavian@protonmail.com>
2021-02-15 14:38:39 +0200

    Moved to SLN

---
+++ CoreEmbedManaged/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreEmbedManaged
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
